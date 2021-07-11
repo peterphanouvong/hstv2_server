12fb957262a7f348d87f311b1b74c72e7a7eeab2
Peter Phanouvong <peterphanouvong@gmail.com>
2021-07-11 11:44:02 +1000

    connect to the correct url

--- db/db.go
+++ db/db.go
@@ -25,10 +25,12 @@ func Initialize(username, password, database, dbHost string) (Database, error) {
 	// db_url := `postgresql://peterhst:tomtom123@localhost:5432/hst?sslmode=disable`
 	// fmt.Println(db_url)
 
+	db_url := `postgres://kdcnxxlaarepgx:1c7205084e7d02c28e1ce255706f57d26635252b0b7155000918f63a01921b9e@ec2-52-86-25-51.compute-1.amazonaws.com:5432/dceq2k4adeamp1`
+
 	dbUri := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, database, password)
 	fmt.Println(dbUri)
 
-	conn, err := pgxpool.Connect(context.Background(), dbUri)
+	conn, err := pgxpool.Connect(context.Background(), db_url)
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
